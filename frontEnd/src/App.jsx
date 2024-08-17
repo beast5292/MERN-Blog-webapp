05f018332be5c5919dc5b0573d20be34fd027a0f
Mindula <mindula5292@gmail.com>
2024-08-17 21:09:10 +0530

    pages are added

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Signup from './pages/Signup.jsx';
+import Signin from './pages/Signin.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+import Projects from './pages/Projects.jsx';
 
 export default function App() {
   return (
-    <div className='text-3xl text-red-500'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-up' element={<Signup />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
